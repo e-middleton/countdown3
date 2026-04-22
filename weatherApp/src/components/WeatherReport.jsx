1a6dafd16b99693648775d9b35f79a68a04cb601
Emily Middleton <emiddleton@smith.edu>
2026-04-22 18:40:01 -0400

    parse week data

--- weatherApp/src/components/WeatherReport.jsx
+++ weatherApp/src/components/WeatherReport.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import HourlyReport from './HourlyReport';
+import SevenDayWeather from './SevenDayWeather';
 
 // const tmp = '{"data":{"timelines":[{"timestep":"1d","endTime":"2026-04-19T11:00:00Z","startTime":"2026-04-14T11:00:00Z","intervals":[{"startTime":"2026-04-14T11:00:00Z","values":{"precipitationProbability":0,"temperature":62.35,"temperatureApparent":62.3,"weatherCode":1000}},{"startTime":"2026-04-15T11:00:00Z","values":{"precipitationProbability":0,"temperature":66.61,"temperatureApparent":66.6,"weatherCode":1001}},{"startTime":"2026-04-16T11:00:00Z","values":{"precipitationProbability":0,"temperature":72.86,"temperatureApparent":72.9,"weatherCode":1000}},{"startTime":"2026-04-17T11:00:00Z","values":{"precipitationProbability":35,"temperature":73.02,"temperatureApparent":73,"weatherCode":1001}},{"startTime":"2026-04-18T11:00:00Z","values":{"precipitationProbability":0,"temperature":40.06,"temperatureApparent":40.1,"weatherCode":1001}},{"startTime":"2026-04-19T11:00:00Z","values":{"precipitationProbability":0,"temperature":55.67,"temperatureApparent":55.7,"weatherCode":1000}}]}]}}'
 // const obj = JSON.parse(tmp);
@@ -11,9 +12,9 @@ const WeatherReport = ( {locationData, city, state, country} ) => {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [hourlyWeather, setHourlyWeather] = useState(null); // for the next day
   const [weatherDescription, setWeatherDescription] = useState(null);
+  const [weeklyWeather, setWeeklyWeather] = useState(null);
 
   useEffect(() => {
-    console.log(currentWeather);
 
     // // for testing
     // setCurrentWeather(weather.data.timelines[0].intervals[0].values); // current weather 
@@ -29,8 +30,37 @@ const WeatherReport = ( {locationData, city, state, country} ) => {
     //   weather.data.timelines[0].intervals[9],
     //   weather.data.timelines[0].intervals[10],
     //   weather.data.timelines[0].intervals[11],
-    //   weather.data.timelines[0].intervals[12],
+    //   weather.data.timelines[0].intervals[12]
+    // ]);
+    // console.log([weather.data.timelines[0].intervals[1].startTime,
+    //   weather.data.timelines[0].intervals[2].startTime,
+    //   weather.data.timelines[0].intervals[3].startTime,
+    //   weather.data.timelines[0].intervals[4].startTime,
+    //   weather.data.timelines[0].intervals[5].startTime,
+    //   weather.data.timelines[0].intervals[6].startTime,
+    //   weather.data.timelines[0].intervals[7].startTime,
+    //   weather.data.timelines[0].intervals[8].startTime,
+    //   weather.data.timelines[0].intervals[9].startTime,
+    //   weather.data.timelines[0].intervals[10].startTime,
+    //   weather.data.timelines[0].intervals[11].startTime,
+    //   weather.data.timelines[0].intervals[12].startTime
     // ])
+
+    // const weekData = [];
+    // for (let j = 0; j < 5; j++) {   // loop through 6 day
+    //   let high = weather.data.timelines[0].intervals[24*j].values.temperature;
+    //   let low = weather.data.timelines[0].intervals[24*j].values.temperature;
+    //   let date = weather.data.timelines[0].intervals[24*j].startTime;
+
+    //   for (let i = 0; i < 24; i++){   // loop through 24 hours
+    //     // console.log(weather.data.timelines[0].intervals[(24*j)+i].values.temperature);
+    //     let currTemp = weather.data.timelines[0].intervals[(24*j) + i].values.temperature;
+    //     if (currTemp > high)  high = currTemp;
+    //     if (currTemp < low) low = currTemp;
+    //   }
+    //   weekData.push({date: date, H:high, L:low})
+    // }
+    // setWeeklyWeather(weekData);
 
     // setWeatherDescription(description["weatherCode"][weather.data.timelines[0].intervals[0].values.weatherCode].toLowerCase());
     // console.log(description["weatherCode"][weather.data.timelines[0].intervals[0].values.weatherCode].toLowerCase().replaceAll(' ', '_'));
@@ -49,7 +79,7 @@ const WeatherReport = ( {locationData, city, state, country} ) => {
     fetch(url, options)
       .then(response => response.json())
       .then(output => {
-        // console.log(output);
+        console.log(output);
         // const jsonString = JSON.stringify(output);
         // console.log(jsonString);
         setCurrentWeather(output.data.timelines[0].intervals[0].values); // current weather 
@@ -68,6 +98,23 @@ const WeatherReport = ( {locationData, city, state, country} ) => {
           output.data.timelines[0].intervals[12],
         ])
         setWeatherDescription(description["weatherCode"][output.data.timelines[0].intervals[0].values.weatherCode].toLowerCase());
+
+        const weekData = [];
+        for (let j = 0; j < 5; j++) {   // loop through 6 day
+          let high = output.data.timelines[0].intervals[24*j].values.temperature;
+          let low = output.data.timelines[0].intervals[24*j].values.temperature;
+          let date = output.data.timelines[0].intervals[24*j].startTime.split("T")[0];
+          let weatherCode = output.data.timelines[0].intervals[24*j].values.weatherCode;
+
+          for (let i = 0; i < 24; i++){   // loop through 24 hours
+            // console.log(weather.data.timelines[0].intervals[(24*j)+i].values.temperature);
+            let currTemp = output.data.timelines[0].intervals[(24*j) + i].values.temperature;
+            if (currTemp > high)  high = currTemp;
+            if (currTemp < low) low = currTemp;
+          }
+          weekData.push({date: date, H:high, L:low, weatherCode:weatherCode})
+        }
+        setWeeklyWeather(weekData);
       })
       .catch(err => console.error(err));
   }, [locationData])
@@ -78,7 +125,7 @@ const WeatherReport = ( {locationData, city, state, country} ) => {
         <h3> { weatherDescription ? `The current weather is ${weatherDescription}` : "Please enter a valid location" }</h3>
         <figure className='weatherImage'>
           <img style={{maxWidth:'30%'}} 
-            src={imageName[currentWeather.weatherCode] ? `./src/assets/tomorrow-weather-codes/V2_icons/large/png/${currentWeather.weatherCode}0_${imageName[currentWeather.weatherCode]}_large.png` : 
+            src={currentWeather ? `./src/assets/tomorrow-weather-codes/V2_icons/large/png/${currentWeather.weatherCode}0_${imageName[currentWeather.weatherCode]}_large.png` : 
             null} 
             alt="weather symbol" 
           />
@@ -93,12 +140,11 @@ const WeatherReport = ( {locationData, city, state, country} ) => {
           <p> {currentWeather ? `though it feels like ${currentWeather.temperatureApparent}` : null }</p>
           <p> {currentWeather ? `There is a ${currentWeather.precipitationProbability + "\u0025"} chance of rain` : null }</p>
         </div>
-      </div>
-      
-      <div>
+
         <h3>The temperature over the next 12 hours: </h3>
         {hourlyWeather? <HourlyReport hourlyData={hourlyWeather} /> : null}
       </div>
+      {weeklyWeather ? <SevenDayWeather weeklyData={weeklyWeather}/> : null}
     </>
   );
 }
